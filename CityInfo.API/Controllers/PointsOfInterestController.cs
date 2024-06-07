abd65857ddcbc1acb8c73c7c2bf73a2dba3d02d8
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-06-07 13:17:41 +0200

    Feat: Add PointsOfInterestController

---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,39 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointsOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{pointofinterestid}")]
+        public ActionResult<IEnumerable<PointsOfInterestDto>> GetPointsOfInterest(int cityId, int pointofinterestid)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            // find point of interest
+            var point = city.PointsOfInterest.FirstOrDefault(x => x.Id == pointofinterestid);
+            if (point == null)
+            {
+                return NotFound();
+            }
+            return Ok(point);
+        }
+    }
+}

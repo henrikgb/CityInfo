23fc12b25ebfa9cc147cb33cb7bcc36e987569ac
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-06-07 12:54:30 +0200

    Update PointsOfInterestDto.cs

--- CityInfo.API/Models/PointsOfInterestDto.cs
+++ CityInfo.API/Models/PointsOfInterestDto.cs
@@ -5,15 +5,5 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; }
-
-        public int NumberOfPointsOfInterest
-        {
-            get
-            {
-                return PointsOfInterests.Count;
-            }
-        }
-
-        public ICollection<PointsOfInterestDto> PointsOfInterests { get; set; } = new List<PointsOfInterestDto>();
     }
 }

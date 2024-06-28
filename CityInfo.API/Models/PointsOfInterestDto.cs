6bb0c45b76e7e95bc5af7a69cc41e23c0b0312cd
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-06-28 22:15:10 +0200

    test branch

--- CityInfo.API/Models/PointsOfInterestDto.cs
+++ CityInfo.API/Models/PointsOfInterestDto.cs
@@ -2,6 +2,7 @@
 {
     public class PointsOfInterestDto
     {
+        // TEST BRANCH
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; }

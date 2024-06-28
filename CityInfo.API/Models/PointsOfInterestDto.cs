8447088e3513d59fa3212aadd748544ee940ea80
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-06-28 22:16:12 +0200

    delete comment

--- CityInfo.API/Models/PointsOfInterestDto.cs
+++ CityInfo.API/Models/PointsOfInterestDto.cs
@@ -2,7 +2,6 @@
 {
     public class PointsOfInterestDto
     {
-        // TEST BRANCH
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; }

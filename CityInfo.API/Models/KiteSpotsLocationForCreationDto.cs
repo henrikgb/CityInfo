3c6845c6b206e8e273fbcd95c68d64fca77849b3
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-07-01 14:23:44 +0200

    feat: Validate input

--- CityInfo.API/Models/KiteSpotsLocationForCreationDto.cs
+++ CityInfo.API/Models/KiteSpotsLocationForCreationDto.cs
@@ -1,9 +1,17 @@
-﻿namespace CityInfo.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
 {
     public class KiteSpotsLocationForCreationDto
     {
+        [Required]
+        [MaxLength(50)]
         public string NameId { get; set; } = string.Empty;
+        [Required]
+        [Range(-90, 90, ErrorMessage = "Latitude must be between -90 and 90.")]
         public double Latitude { get; set; }
+        [Required]
+        [Range(-180, 180, ErrorMessage = "Longitude must be between -180 and 180.")]
         public double Longitude { get; set; }
     }
 }

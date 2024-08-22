6f0a088f2a36d5b60c4d4a1705bbd882680c3d23
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-08-22 12:25:20 +0200

    Feat: Introduce microsoft.entityframeworkcore, and add entities, dbcontexts and database migrations for city and pointsofinterest

---
+++ CityInfo.API/Entities/City.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities
+{
+    public class City
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Generating primary keys
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+        public ICollection<PointOfInterest> PointsOfInterest { get; set; }
+               = new List<PointOfInterest>();
+
+        // Name should not have a null value...
+        // We want City to always have a name...
+        public City(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Generating primary keys
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+        [ForeignKey("CityId")]
+        public City? City { get; set; }
+        public int CityId { get; set; }
+
+        // Name should not have a null value...
+        // We want PointOfInterest to always have a name...
+        public PointOfInterest(string name)
+        {
+            Name = name;
+        }
+    }
+}
